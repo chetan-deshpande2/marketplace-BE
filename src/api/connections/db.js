4d4b0a9869c074870716ca3c83fc54b6520a269a
Chetan Deshpande <chetan.deshpande@protonmail.com>
2022-12-29 12:59:56 +0530

    updated code

--- src/api/connections/db.js
+++ src/api/connections/db.js
@@ -1,38 +1,19 @@
 /* eslint-disable no-undef */
-import mongoose from "mongoose";
-import config from "../config/index";
-import logger from "../middleware/logger";
-mongoose.connect(config.db.str, config.db.options);
+import mongoose from 'mongoose';
+import dontenv from 'dotenv';
+dontenv.config();
+
+const connectionURI = process.env.MONGODB_URL;
+const options = {
+  auto_reconnect: true,
+  poolSize: 200,
+  useNewUrlParser: true,
+  readPreference: 'primaryPreferred',
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+mongoose.connect(connectionURI, options);
 const db = mongoose.connection;
-
-db.on("disconnected", () => {
-  logger.debug("Mongoose connection disconnected for master DB");
-});
-
-db.on("connected", () => {
-  logger.info("Mongoose connection open to master DB");
-});
-
-// If the connection throws an error
-db.on("error", (err) => {
-  logger.debug(`Mongoose connection error for master DB: ${err}`);
-});
-
-// When the connection is disconnected
-
-// When connection is reconnected
-db.on("reconnected", () => {
-  logger.info("Mongoose connection reconnected for master DB");
-});
-// If the Node process ends, close the Mongoose connection
-process.on("SIGINT", () => {
-  db.close(() => {
-    logger.debug(
-      "Mongoose connection disconnected for master DB through app termination"
-    );
-    // eslint-disable-next-line no-process-exit
-    process.exit(0);
-  });
-});
 
 export default db;
